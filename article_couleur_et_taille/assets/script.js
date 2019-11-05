5d6887e48ffc76fbe1f20cbeb2e371b001936c40
Livradoch <55798903+Livradoch@users.noreply.github.com>
2019-11-05 13:13:36 +0100

    Add files via upload

--- article_couleur_et_taille/assets/script.js
+++ article_couleur_et_taille/assets/script.js
@@ -1,3 +1,26 @@
-let color= document.querySelector("#color")
-let taille= document.querySelector("#taille")
+let green= document.querySelector("#couleurgreen")
+let yellow= document.querySelector("#couleuryellow")
+let purple= document.querySelector("#couleurpurple")
+let XL= document.querySelector("#tailleXL")
+let L= document.querySelector("#tailleL")
+let M= document.querySelector("#tailleM")
+let S= document.querySelector("#tailleS")
+let button= document.querySelectorAll(".button");
+console.log(button);
+
+
+for (let i = 0; i < button.length; i++) {
+    button[i].addEventListener('click', function () {
+        alert('hello')
+    });
+    
+}
+// button.addEventListener('click', function(){
+// switch {
+//     if () {
+//         get
+//     }
+// }
+
+// });
 
